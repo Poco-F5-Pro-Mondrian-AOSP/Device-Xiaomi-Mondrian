527185795aee23f9793a9b3c40950792fa99c43d
DanipunK1 <pegate.eltrueco@gmail.com>
2024-04-18 23:53:19 +0200

    mondrian: Add Gcam

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/mondrian/mondrian-vendor.mk)
 
+# Google Camera
+$(call inherit-product-if-exists, vendor/Gcam/config.mk)
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMondrian \
